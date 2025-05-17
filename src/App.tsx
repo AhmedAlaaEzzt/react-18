d344baf2dd6c013ad33080565b8abc390eeeb726
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2025-05-17 09:58:41 +0300

    00008 starting point

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { StudentForm } from "./components/StudentForm";
+import { useState } from "react";
+import { Item } from "./components/Item";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="flex justify-center h-screen  items-center">
-      <StudentForm />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="flex flex-col justify-start items-center">
+        <h1>Count: {count}</h1>
+        <button
+          className="p-2 bg-blue-600 rounded text-white"
+          onClick={() => setCount(count + 1)}
+        >
+          Increment
+        </button>
+        <Item />
+      </div>
     </div>
   );
 }
---
+++ src/components/Item.tsx
@@ -0,0 +1,4 @@
+export const Item = () => {
+  console.log("Item Rendered");
+  return <></>;
+};
