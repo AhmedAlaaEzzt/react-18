b5f521b48e31f3fb53f6a4063c0973ec71e33a52
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2025-06-24 02:30:37 +0300

    00010 starting point

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import { useState } from "react";
-import Item from "./components/Item";
+import { Card } from "./components/Card";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const fn = () => {
-    console.log("I am fn");
-  };
-
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="flex flex-col justify-start items-center">
-        <h1>Count: {count}</h1>
-        <button
-          className="p-2 bg-blue-600 rounded text-white"
-          onClick={() => setCount(count + 1)}
-        >
-          Increment
-        </button>
-        <Item fun={fn} />
+        <Card />
+        <h2>Welcome</h2>
+        <p>This is a reusable card component using children prop.</p>
       </div>
     </div>
   );
---
+++ src/components/Card.tsx
@@ -0,0 +1,5 @@
+export const Card = () => {
+  return (
+    <div className="border-2 p-2 rounded shadow-md">I'm a card component</div>
+  );
+};
