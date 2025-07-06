f4e6a44835cfa8f51d7994ee188b3d8d3a71deb2
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2025-07-06 15:34:50 +0300

    00013 starting point

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import { Card } from "./components/Card";
-import "./App.css";
-
 function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <div className="flex flex-col justify-start items-center">
-        <Card>
-          <h2>Welcome</h2>
-          <p>This is a reusable card component using children prop.</p>
-        </Card>
-      </div>
+      <h1>This is my App</h1>
     </div>
   );
 }
--- src/components/Card.tsx
+++
@@ -1,5 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export const Card = ({ children }: PropsWithChildren) => {
-  return <div className="border-2 p-2 rounded shadow-md">{children}</div>;
-};
