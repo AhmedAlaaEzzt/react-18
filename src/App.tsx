f75c71a253f57a109477a4b7aa0b7ded07a4d8be
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2025-07-06 15:45:41 +0300

    00013 starting point

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <h1>This is my App</h1>
+      <h1>Replace the heading with a table showing a static user's details.</h1>
     </div>
   );
 }
