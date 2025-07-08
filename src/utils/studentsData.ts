fe8d4789ba20da7efa0bfb69cd9fdef3f0462dc2
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2025-07-08 20:55:19 +0300

    00014 starting point]

--- src/utils/studentsData.ts
+++ src/utils/studentsData.ts
@@ -1,12 +1,12 @@
 export const students = [
-  { id: 1, fullName: "Ahmed Alaa", age: 16, email: "ahmed@gmail.com", class: "React" },
-  { id: 2, fullName: "Sara Mohamed", age: 17, email: "sara@gmail.com", class: "React" },
-  { id: 3, fullName: "Omar Hassan", age: 18, email: "omar@gmail.com", class: "React" },
-  { id: 4, fullName: "Laila Youssef", age: 19, email: "laila@gmail.com", class: "React" },
-  { id: 5, fullName: "Mohamed Tarek", age: 20, email: "mohamed@gmail.com", class: "React" },
-  { id: 6, fullName: "Mona Adel", age: 21, email: "mona@gmail.com", class: "React" },
-  { id: 7, fullName: "Khaled Nabil", age: 22, email: "khaled@gmail.com", class: "React" },
-  { id: 8, fullName: "Nour Samir", age: 23, email: "nour@gmail.com", class: "React" },
-  { id: 9, fullName: "Yasmine Farid", age: 24, email: "yasmine@gmail.com", class: "React" },
-  { id: 10, fullName: "Tamer Saeed", age: 25, email: "tamer@gmail.com", class: "React" }
+  { id: 1, fullName: "Ahmed Alaa", age: 16, email: "ahmed@gmail.com", enrolledClass: "React" },
+  { id: 2, fullName: "Sara Mohamed", age: 17, email: "sara@gmail.com", enrolledClass: "React" },
+  { id: 3, fullName: "Omar Hassan", age: 18, email: "omar@gmail.com", enrolledClass: "React" },
+  { id: 4, fullName: "Laila Youssef", age: 19, email: "laila@gmail.com", enrolledClass: "React" },
+  { id: 5, fullName: "Mohamed Tarek", age: 20, email: "mohamed@gmail.com", enrolledClass: "React" },
+  { id: 6, fullName: "Mona Adel", age: 21, email: "mona@gmail.com", enrolledClass: "React" },
+  { id: 7, fullName: "Khaled Nabil", age: 22, email: "khaled@gmail.com", enrolledClass: "React" },
+  { id: 8, fullName: "Nour Samir", age: 23, email: "nour@gmail.com", enrolledClass: "React" },
+  { id: 9, fullName: "Yasmine Farid", age: 24, email: "yasmine@gmail.com", enrolledClass: "React" },
+  { id: 10, fullName: "Tamer Saeed", age: 25, email: "tamer@gmail.com", enrolledClass: "React" }
 ]
