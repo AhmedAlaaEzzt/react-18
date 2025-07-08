7b15b8bea83d93ab983ffffe106892ab87b5f1f0
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2025-07-08 20:19:25 +0300

    00014 starting point

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
+import { Table } from "./components/Table";
+
 function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <table>
-        <thead>
-          <tr>
-            <th>Full Name</th>
-            <th>Age</th>
-            <th>Email</th>
-            <th>Class</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Mohamed</td>
-            <td>30</td>
-            <td>Ahmed@gmail.com</td>
-            <td>MBA</td>
-          </tr>
-        </tbody>
-      </table>
+      <Table />
     </div>
   );
 }
---
+++ src/components/Table.tsx
@@ -0,0 +1,22 @@
+export const Table = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Full Name</th>
+          <th>Age</th>
+          <th>Email</th>
+          <th>Class</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Mohamed</td>
+          <td>30</td>
+          <td>Ahmed@gmail.com</td>
+          <td>MBA</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
