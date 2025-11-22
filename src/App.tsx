0076d2b3d4d5c8490da47536d29f28841a8d83d9
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2025-11-22 10:40:48 +0200

    00019 starting point

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Box, Button, Stack } from "@mui/material";
 import { StudentTable } from "./components/StudentTable";
 
 function App() {
-  return <StudentTable />;
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+      <Stack direction="row" justifyContent="flex-end">
+        <Button variant="contained">Add Student</Button>
+      </Stack>
+      <StudentTable />
+    </Box>
+  );
 }
 
 export default App;
