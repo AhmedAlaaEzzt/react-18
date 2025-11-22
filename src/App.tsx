54f6b3a2c98882365e1d20a64fa83aaf11c2edfc
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2025-11-22 12:44:29 +0200

    00020 starting point

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Box, Button, Stack } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 import { StudentTable } from "./components/StudentTable";
 
 function App() {
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
       <Stack direction="row" justifyContent="flex-end">
-        <Button variant="contained">Add Student</Button>
+        <Button variant="contained" startIcon={<AddIcon />}>
+          Add Student
+        </Button>
       </Stack>
       <StudentTable />
     </Box>
