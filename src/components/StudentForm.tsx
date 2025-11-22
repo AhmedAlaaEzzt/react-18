dd2f78d80ed733b9c060b39efc1abbf6324f6748
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2025-11-22 18:21:58 +0200

    00020 starting point updated StudentForm

--- src/components/StudentForm.tsx
+++ src/components/StudentForm.tsx
@@ -1,22 +1,14 @@
-import Paper from "@mui/material/Paper";
 import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
+
+import { Stack } from "@mui/material";
 
 export const StudentForm = () => {
   return (
-    <Paper
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: "10px",
-        padding: "20px",
-      }}
-    >
+    <Stack sx={{ p: 1 }} direction={"column"} gap={2}>
       <TextField id="outlined-basic" label="Full Name" variant="outlined" />
       <TextField id="outlined-basic" label="Age" variant="outlined" />
       <TextField id="outlined-basic" label="Email" variant="outlined" />
       <TextField id="outlined-basic" label="Class" variant="outlined" />
-      <Button variant="contained">Save</Button>
-    </Paper>
+    </Stack>
   );
 };
