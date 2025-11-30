af326ccae4d97a6c574c350c1c2485b12595c0a3
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2025-11-30 11:51:49 +0200

    00022 starting point

--- src/components/StudentForm.tsx
+++ src/components/StudentForm.tsx
@@ -1,14 +1,21 @@
 import TextField from "@mui/material/TextField";
 
 import { Stack } from "@mui/material";
+import  { ChangeEvent } from "react";
 
 export const StudentForm = () => {
+
+
+  const handelChange = (e: ChangeEvent<HTMLInputElement>) =>{
+    console.log(e.target.name,e.target.value)
+  }
+
   return (
     <Stack sx={{ pt: 1 }} direction={"column"} gap={2}>
-      <TextField id="outlined-basic" label="Full Name" variant="outlined" />
-      <TextField id="outlined-basic" label="Age" variant="outlined" />
-      <TextField id="outlined-basic" label="Email" variant="outlined" />
-      <TextField id="outlined-basic" label="Class" variant="outlined" />
+      <TextField name="fullName"  id="full-name" label="Full Name" variant="outlined" onChange={handelChange}/>
+      <TextField name="age" id="age" label="Age" variant="outlined" onChange={handelChange} />
+      <TextField name="email" id="email" label="Email" variant="outlined" onChange={handelChange}/>
+      <TextField name="enrolledClass" id="class" label="Class" variant="outlined" onChange={handelChange}/>
     </Stack>
   );
 };
